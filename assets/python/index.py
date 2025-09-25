dde6d156c5eca93df39ebc92e5b8635cc06ad7e5
Ahmdgamal7 <ahmedgamalsharafeldeen9@gmail.com>
2025-09-25 18:27:11 +0300

    Add files via upload

---
+++ assets/python/index.py
@@ -0,0 +1,63 @@
+vector1=np.aray([1,2,3,])
+vector2=np.aray([10],[20],[30])
+print(vector1)
+print("Horizintal vector")
+list1=[1,2,3]
+list2=[[10],[20],[30]]
+vector1=np.array(list1)
+vector2=np.array(list2)
+#ن
+list=np.arange(start=1,stop=13,stop=1)
+print('vector')
+print(list)
+3
+arr=np.array(ist.r)
+#shjfhhffjkkkk
+vector1=np.aray([1,2,3,])
+vector2=np.aray([10],[20],[30])
+x=vector1+vector2
+y=vector2-vector1
+
+from numby import aray
+form numby.linalg import norm
+arr=array([1,2,3,4,5,])
+print(arr)
+norm_11=norm(arr,1)
+print(norm_11)
+33333333333333333
+n_array=np.array([20,10],[30,10])
+print("Numby matrix is")
+print(n_array)
+det=np.linalg.det(n_array)
+print("\nDetermine of given 2*2")
+print(int(det))
+A_inv=np.linalg.inv(A)
+print(A_inv)
+
+def product = 0
+vector1 =np.array(vector1)
+vector2=np.array(vector2)
+return np.dot(vector1,vector2)==0
+vector1 = ([10],[30])
+vector2 = ([30],[20])
+print("is dont product zero")
+311111111111
+
+import numpy as np
+from numpy.linalg import eig
+a= np.array([2,9],[3,4])
+W,V=eig(a)
+print(eue,W)
+print(fkjfje,V)
+0333293
+printf(data.info())
+data ['rest'].sum()
+data['rest']
+data['rest']=data['rest'].strip('%')
+hhhhhhhhhhhhh
+data.drop(data[data['rating']>5.index,implace = True)
+          jddddddddddd
+          data.loc[data['Rating']>5 , 'Rating']=5
+          vfffvvfddddddddddd
+          assert data[ 'Rating'].max()<=5 
+          ghhhhhhghhhhhhhhhhhhhhhhh
